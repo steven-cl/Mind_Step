f2e2275a07aea665c69bd68d089d794f27a6b6f9
steven-cl <castillsteven12@gmail.com>
2023-09-28 10:36:37 -0600

    Creacion de las activities principales

---
+++ app/src/main/java/com/project/mindstep/Paciente/Test.kt
@@ -0,0 +1,12 @@
+package com.project.mindstep.Paciente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.mindstep.R
+
+class Test : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
---
+++ app/src/main/java/com/project/mindstep/TestCreator/AdministrarTest.kt
@@ -0,0 +1,12 @@
+package com.project.mindstep.TestCreator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.mindstep.R
+
+class AdministrarTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_administrar_test)
+    }
+}
